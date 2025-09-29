b3f997079468f9860c497ffae58df9d44c8c3336
tamagoman <dierocam@gmail.com>
2025-09-29 13:47:06 -0500

    Show a constant example in the screen

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,6 +1,7 @@
 
 
 
+import 'package:cinemapedia/config/constants/environment.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -10,8 +11,10 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Placeholder(),
+    return Scaffold(
+      body: Center(
+        child: Text( Environment.theMovieDbKey ),
+      ),
     );
   }
 }
