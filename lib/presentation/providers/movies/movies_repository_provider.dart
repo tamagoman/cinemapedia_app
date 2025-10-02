294e5e4adbe19d2cdb41d24b563697db61b1279a
tamagoman <dierocam@gmail.com>
2025-10-02 18:13:12 -0500

    Create the movie repository provider

---
+++ lib/presentation/providers/movies/movies_repository_provider.dart
@@ -0,0 +1,10 @@
+
+import 'package:cinemapedia/infrastructure/datasources/moviedb_datasource.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:cinemapedia/infrastructure/repositories/movie_repository_impl.dart';
+
+// This repository is inmutable 
+final movieRepositoryProvider = Provider((_ref){
+
+  return MovieRepositoryImpl( MoviedbDatasource() );
+});
