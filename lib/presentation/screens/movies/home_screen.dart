2b88498488dee940ec0fca1438f7ec8fe2b1bba0
tamagoman <dierocam@gmail.com>
2025-10-04 22:21:55 -0500

    First presentation of the movies list

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,8 +1,8 @@
 
 
-
-import 'package:cinemapedia/config/constants/environment.dart';
+import 'package:cinemapedia/presentation/providers/providers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class HomeScreen extends StatelessWidget {
 
@@ -11,9 +11,48 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return _HomeView();
+  }
+}
+
+class _HomeView extends ConsumerStatefulWidget {
+  const _HomeView();
+
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends ConsumerState<_HomeView> {
+
+  @override
+  void initState() {
+    super.initState();
+
+    ref.read( nowPlayingMoviesProvider.notifier ).loadNextPage();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final nowPlayingMovies = ref.watch( nowPlayingMoviesProvider );
+
+    if( nowPlayingMovies.length == 0 ) {
+      return Scaffold(
+        body: Center(
+          child: CircularProgressIndicator()
+        ),
+      );
+    };
+
     return Scaffold(
-      body: Center(
-        child: Text( Environment.theMovieDbKey ),
+      body: ListView.builder(
+        itemCount: nowPlayingMovies.length,
+        itemBuilder: (context, index) {
+          final movie = nowPlayingMovies[index];
+      
+          return ListTile(
+            title: Text( movie.title ),
+          );
+        },
       ),
     );
   }
