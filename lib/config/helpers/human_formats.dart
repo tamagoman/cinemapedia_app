fa270febcafaa0fe859ddacb1b079097b664dcc5
tamagoman <dierocam@gmail.com>
2025-11-13 15:46:54 -0500

    Add

---
+++ lib/config/helpers/human_formats.dart
@@ -0,0 +1,15 @@
+
+
+import 'package:intl/intl.dart';
+
+class HumanFormats {
+  static String number(double number) {
+    final formatterNumber = NumberFormat.compactCurrency(
+      decimalDigits: 0,
+      symbol: '',
+      locale: 'en'
+    );
+
+    return formatterNumber.format(number);
+  }
+}
