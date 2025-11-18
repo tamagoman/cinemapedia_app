9b16ccea75acd9ea7eb6bb466341959fc04b0107
tamagoman <dierocam@gmail.com>
2025-11-17 22:13:54 -0500

    Change _ref to ref in RepositoryProvider

--- lib/presentation/providers/movies/movies_repository_provider.dart
+++ lib/presentation/providers/movies/movies_repository_provider.dart
@@ -4,7 +4,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:cinemapedia/infrastructure/repositories/movie_repository_impl.dart';
 
 // This repository is inmutable 
-final movieRepositoryProvider = Provider((_ref){
+final movieRepositoryProvider = Provider((ref){
 
   return MovieRepositoryImpl( MoviedbDatasource() );
 });
