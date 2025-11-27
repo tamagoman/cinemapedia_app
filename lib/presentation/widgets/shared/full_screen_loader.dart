79d33cc513b8a29c16a1d38052ea53899519e5d2
tamagoman <dierocam@gmail.com>
2025-11-26 22:57:10 -0500

    Create the full screen loader

---
+++ lib/presentation/widgets/shared/full_screen_loader.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class FullScreenLoader extends StatelessWidget {
+  const FullScreenLoader({super.key});
+
+
+  Stream<String> getLoadingMessages() {
+    final messages = <String>[
+      'Cargando contenido...',
+      'Por favor, espere un momento...',
+      'Estamos preparando todo para ti...',
+      'Cargando, casi listo...',
+      'Un momento por favor, estamos trabajando en ello...'
+    ];
+
+    return Stream.periodic(
+      const Duration(milliseconds: 1200),
+      (step) => messages[step],
+    ).take(messages.length);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return 
+    Scaffold(
+      body:Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Espere por favor'),
+            const SizedBox(height: 10),
+            const CircularProgressIndicator(strokeWidth: 2,),
+            const SizedBox(height: 10),
+
+            StreamBuilder(
+              stream: getLoadingMessages(),
+              builder: (context, snapshot){
+                if(!snapshot.hasData) {
+                  return const Text('Cargando...');
+                }
+
+                return Text(snapshot.data!);
+              }
+            )
+          ],
+        ),
+      )
+    );
+  }
+}
