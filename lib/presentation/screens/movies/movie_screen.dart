29167b441fcc9a7fd99944f935d1e74a529954f2
tamagoman <dierocam@gmail.com>
2026-01-14 22:29:06 -0500

    Initial version of movie screen

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -37,12 +37,92 @@ class MovieScreenState extends ConsumerState<MovieScreen> {
       );
     }
     return Scaffold(
-      appBar: AppBar(
-        title: Text('MovieID: ${widget.movieId}'),
-      ),
-      body: const Center(
-        child: Text('Movie Details Here'),
-      ),
+      body: CustomScrollView(
+        physics: const ClampingScrollPhysics(),
+        slivers: [
+          _CustomSliverAppBar(movie: movie),
+        ]
+      )
     );
-  }
+  }  
 }
+
+
+
+class _CustomSliverAppBar extends StatelessWidget {
+    final Movie movie;
+
+    const _CustomSliverAppBar({
+      required this.movie,
+    });
+
+    @override
+    Widget build(BuildContext context) {
+      final size = MediaQuery.of(context).size;
+      return SliverAppBar(
+        backgroundColor: Colors.black,
+        expandedHeight: size.height * 0.7,
+        foregroundColor: Colors.white,
+        flexibleSpace: FlexibleSpaceBar(
+          titlePadding: const EdgeInsets.symmetric( horizontal: 10, vertical: 5),
+          title: Text( 
+            movie.title,
+            style: const TextStyle( 
+              fontSize: 20,
+              color: Colors.white
+              // overflow: TextOverflow.ellipsis
+            ),
+            textAlign: TextAlign.start,
+            
+          ),
+          background: Stack(
+            children: [
+              SizedBox.expand(
+                child: Image.network(
+                  movie.posterPath,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              const SizedBox.expand(
+                child: DecoratedBox(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      stops: [0.7, 1.0],
+                      colors: <Color>[
+                        Colors.transparent,
+                        Colors.black,
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+
+              const SizedBox.expand(
+                child: DecoratedBox(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topLeft,
+                      stops: [0.0, 0.2],
+                      colors: <Color>[
+                        Colors.black,
+                        Colors.transparent,
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              
+            ],
+          ),
+          // background: FadeInImage(
+          //   placeholder: const AssetImage('assets/loading.gif'),
+          //   image: NetworkImage( movie.posterPath ),
+          //   fit: BoxFit.cover,
+          // ),
+          
+        ),
+      );
+    }
+  } 
